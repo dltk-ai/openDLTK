b4c1a7b3b1018794526da2270d8ab4f52c0b4445
Vivek Kumar Yadav <46562067+vivekkya@users.noreply.github.com>
2021-05-03 16:32:36 +0530

    removed unnecessary code

--- install_docker.py
+++ install_docker.py
@@ -2,26 +2,6 @@ import os
 from shutil import which
 
 
-def install_docker(root_user):
-    if os.name == "posix":
-        print("Installing Docker")
-        install_commands = [f'{root_user} apt-get update',
-                            'curl -fsSL https://get.docker.com -o get-docker.sh',
-                            'sh get-docker.sh',
-                            f'{root_user} curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose',
-                            f'{root_user} chmod +x /usr/local/bin/docker-compose']
-        for command in install_commands:
-            os.system(command)
-
-        print("Installed docker images")
-
-    elif os.name == "nt":
-        print("For installing docker on windows Home machine please refer to https://docs.docker.com/docker-for-windows/install-windows-home/")
-        print("For installing docker on windows Pro, Enterprise please refer to https://docs.docker.com/docker-for-windows/install/")
-
-    return
-
-
 def is_docker_present(name):
     """Check whether `name` is on PATH and marked as executable."""
     print(which(name))
